a44d4267ef7f47d434fe34f0e5257cd4567ca5e3
Muhammad Moeed Ullah <moeedullah12@gmail.com>
2022-02-16 14:11:50 +0500

    feat: added implementation for sqs trigger lambda

--- src/sqs/handler.py
+++ src/sqs/handler.py
@@ -1,14 +1,75 @@
+import hashlib
 import os
 import boto3
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timezone
 client = boto3.client('dynamodb')
 REQUESTS_TABLE = os.environ['REQUESTS_TABLE']
 
-def handler (event, context):
-	resp = client.put_item(
+
+def getHash(string: str):
+    return hashlib.sha256(string.encode()).hexdigest()
+
+def insertToDB(messageId, title, url, contents, failedRecords):
+	try:
+		requestId = getHash(url)
+		date = str(datetime.now().replace(tzinfo=timezone.utc))
+		resp = client.put_item(
         TableName=REQUESTS_TABLE,
         Item={
-            'requestId': {'S': 'asdasdasdasdasd' },
-            'title': {'S': REQUESTS_TABLE },
-			'data':{'S':'asdasdasdad'} })
-	print('resp" ', resp)
-	return
+            'requestId': {'S': requestId },
+            'title': {'S': title },
+			'date':{'S': date} ,
+			'content': {'S':contents},
+			'url': {'S': url}
+			})
+		print('Dynamo Response : ',resp)
+	except Exception as e:
+		print('error while inserting : ', requestId )
+		print('exception : ', e)
+		failedRecords.append(messageId)
+
+def scrapURLElements (url): 
+	try :
+		reqs = requests.get(url)	
+		soup = BeautifulSoup(reqs.text, 'html.parser')	
+		titles = soup.find_all('title')
+		if not titles or len(titles) < 1:
+			titles = ''
+		else:
+			titles = titles[0].get_text()
+		
+		return titles, str(soup)
+	except Exception as e:
+		print('error while scraping : ', url )
+		print('exception : ', e)
+		return '', ''
+
+def extractURL (record, failedRecords): 
+	try :
+		url = record['messageAttributes']['URL']['stringValue']
+		return url
+	except Exception as e:
+		print('error while extracting url from record : ', record )
+		print('exception : ', e)
+		failedRecords.append(record['messageId'])
+		return ''
+
+def handler (event, context):
+	failedRecords = []
+	try :
+		for record in event['Records']:
+			url = extractURL(record, failedRecords)
+			if len(url)>0:
+				title, content = scrapURLElements(url)
+				if title:
+					insertToDB(record['messageId'],title,url,content,failedRecords)
+				else:
+					failedRecords.append(record['messageId'])
+		print("failed records: ", failedRecords)	
+		return failedRecords
+	except Exception as e:
+		print ('Exception : ', e)
+		return ''
+
